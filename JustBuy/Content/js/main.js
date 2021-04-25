d024d0864e40df27481e5d34f0713b675a54ca77
caolinh22 <48510866+caolinh22@users.noreply.github.com>
2021-04-25 16:32:30 +0700

    update account

--- JustBuy/Content/js/main.js
+++ JustBuy/Content/js/main.js
@@ -22,7 +22,7 @@
         4.4 Active Blog
         4.5 Active Blog 2
         4.6 Active Related Product
-		4.7 Active Team Member
+        4.7 Active Team Member
     5. Countdown
     6. ScrollUp
     7. Tooltip 
@@ -42,22 +42,22 @@
 [ End table content ]
 ======================================*/
 
-(function($) {
+(function ($) {
     "use strict";
 
-	/* ********************************************
-		1. jQuery MeanMenu
-	******************************************** */
+    /* ********************************************
+        1. jQuery MeanMenu
+    ******************************************** */
     jQuery('nav#dropdown').meanmenu();
 
-	/* ********************************************
-		2. wow js active
-	******************************************** */
+    /* ********************************************
+        2. wow js active
+    ******************************************** */
     new WOW().init();
 
-	/* ********************************************
-		3. jQuery Nivo Slider (home-2)
-	******************************************** */
+    /* ********************************************
+        3. jQuery Nivo Slider (home-2)
+    ******************************************** */
     $('#nivoslider-2').nivoSlider({
         directionNav: true,
         animSpeed: 1000,
@@ -69,14 +69,14 @@
         prevText: '<i class="zmdi zmdi-long-arrow-up"></i>',
         nextText: '<i class="zmdi zmdi-long-arrow-down"></i>'
     });
-    
-	/* ********************************************
-		4. Slick Carousel 
-	******************************************** */
 
-	/* -------------------------------------
-			4.1 Active Slider - 1 (home-1)
-	------------------------------------- */
+    /* ********************************************
+        4. Slick Carousel 
+    ******************************************** */
+
+    /* -------------------------------------
+            4.1 Active Slider - 1 (home-1)
+    ------------------------------------- */
     $('.active-slider-1').slick({
         autoplay: true,
         autoplaySpeed: 8000,
@@ -88,9 +88,9 @@
         nextArrow: '<button type="button" class="arrow-next"><i class="zmdi zmdi-long-arrow-right"></i></button>',
     });
 
-	/*----------------------------
-			4.2 Active By Brand
-	------------------------------ */  
+    /*----------------------------
+            4.2 Active By Brand
+    ------------------------------ */
     $('.active-by-brand').slick({
         speed: 700,
         arrows: true,
@@ -100,15 +100,15 @@
         prevArrow: '<button type="button" class="arrow-prev"><i class="zmdi zmdi-long-arrow-left"></i></button>',
         nextArrow: '<button type="button" class="arrow-next"><i class="zmdi zmdi-long-arrow-right"></i></button>',
         responsive: [
-            { breakpoint: 991, settings: { slidesToShow: 3 }  },
-            { breakpoint: 767, settings: { slidesToShow: 1 }  },
-            { breakpoint: 479, settings: { slidesToShow: 1 }  }
+            { breakpoint: 991, settings: { slidesToShow: 3 } },
+            { breakpoint: 767, settings: { slidesToShow: 1 } },
+            { breakpoint: 479, settings: { slidesToShow: 1 } }
         ]
     });
-    
-	/*------------------------------------
-			4.3 Active Featured Product
-	----------------------------------- */  
+
+    /*------------------------------------
+            4.3 Active Featured Product
+    ----------------------------------- */
     $('.active-featured-product').slick({
         speed: 700,
         arrows: true,
@@ -118,15 +118,15 @@
         prevArrow: '<button type="button" class="arrow-prev"><i class="zmdi zmdi-long-arrow-left"></i></button>',
         nextArrow: '<button type="button" class="arrow-next"><i class="zmdi zmdi-long-arrow-right"></i></button>',
         responsive: [
-            {  breakpoint: 991,   settings: { slidesToShow: 3,  }  },
-            {  breakpoint: 767,   settings: { slidesToShow: 1, }   },
-            {  breakpoint: 479,   settings: { slidesToShow: 1, }   },
+            { breakpoint: 991, settings: { slidesToShow: 3, } },
+            { breakpoint: 767, settings: { slidesToShow: 1, } },
+            { breakpoint: 479, settings: { slidesToShow: 1, } },
         ]
-    });    
+    });
 
-	/*----------------------------
-			4.4 Active Blog
-	------------------------------ */  
+    /*----------------------------
+            4.4 Active Blog
+    ------------------------------ */
     $('.active-blog').slick({
         speed: 700,
         arrows: false,
@@ -134,15 +134,15 @@
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
-            {  breakpoint: 991,   settings: { slidesToShow: 2,  }  },
-            {  breakpoint: 767,   settings: { slidesToShow: 1, }   },
-            {  breakpoint: 479,   settings: { slidesToShow: 1, }   },
+            { breakpoint: 991, settings: { slidesToShow: 2, } },
+            { breakpoint: 767, settings: { slidesToShow: 1, } },
+            { breakpoint: 479, settings: { slidesToShow: 1, } },
         ]
     });
 
-	/*----------------------------
-			4.5 Active Blog 2
-	------------------------------ */ 
+    /*----------------------------
+            4.5 Active Blog 2
+    ------------------------------ */
     $('.active-blog-2').slick({
         speed: 700,
         arrows: false,
@@ -150,15 +150,15 @@
         slidesToShow: 2,
         slidesToScroll: 1,
         responsive: [
-            {  breakpoint: 991,   settings: { slidesToShow: 2,  }  },
-            {  breakpoint: 767,   settings: { slidesToShow: 1, }   },
-            {  breakpoint: 479,   settings: { slidesToShow: 1, }   },
+            { breakpoint: 991, settings: { slidesToShow: 2, } },
+            { breakpoint: 767, settings: { slidesToShow: 1, } },
+            { breakpoint: 479, settings: { slidesToShow: 1, } },
         ]
     });
 
-	/*------------------------------------
-			4.6 Active Related Product
-	-------------------------------------- */
+    /*------------------------------------
+            4.6 Active Related Product
+    -------------------------------------- */
     $('.active-related-product').slick({
         speed: 700,
         arrows: false,
@@ -166,15 +166,15 @@
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
-            {  breakpoint: 991,   settings: { slidesToShow: 2,  }  },
-            {  breakpoint: 767,   settings: { slidesToShow: 1, }   },
-            {  breakpoint: 479,   settings: { slidesToShow: 1, }   },
+            { breakpoint: 991, settings: { slidesToShow: 2, } },
+            { breakpoint: 767, settings: { slidesToShow: 1, } },
+            { breakpoint: 479, settings: { slidesToShow: 1, } },
         ]
     });
 
-	/*----------------------------
-			4.7 Active Team Member
-	------------------------------ */
+    /*----------------------------
+            4.7 Active Team Member
+    ------------------------------ */
     $('.active-team-member').slick({
         speed: 700,
         arrows: false,
@@ -182,40 +182,40 @@
         slidesToShow: 4,
         slidesToScroll: 1,
         responsive: [
-            {  breakpoint: 991,   settings: { slidesToShow: 2,  }  },
-            {  breakpoint: 767,   settings: { slidesToShow: 1, }   },
-            {  breakpoint: 479,   settings: { slidesToShow: 1, }   },
+            { breakpoint: 991, settings: { slidesToShow: 2, } },
+            { breakpoint: 767, settings: { slidesToShow: 1, } },
+            { breakpoint: 479, settings: { slidesToShow: 1, } },
         ]
     });
 
-	/* ********************************************
-		5. Countdown
-	******************************************** */
-    $('[data-countdown]').each(function() {
+    /* ********************************************
+        5. Countdown
+    ******************************************** */
+    $('[data-countdown]').each(function () {
         var $this = $(this), finalDate = $(this).data('countdown');
-        $this.countdown(finalDate, function(event) {
+        $this.countdown(finalDate, function (event) {
             $this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Mint</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Sec</p></span>'));
         });
-    }); 
+    });
 
-	/* ********************************************
-		6. ScrollUp
-	******************************************** */
-	$.scrollUp({
-		scrollText: '<i class="zmdi zmdi-chevron-up"></i>',
-		easingType: 'linear',
-		scrollSpeed: 900,
-		animation: 'fade'
-	}); 
+    /* ********************************************
+        6. ScrollUp
+    ******************************************** */
+    $.scrollUp({
+        scrollText: '<i class="zmdi zmdi-chevron-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
 
-	/* ********************************************
-		7. Tooltip 
-	******************************************** */
-    $('[data-toggle="tooltip"]').tooltip(); 
+    /* ********************************************
+        7. Tooltip 
+    ******************************************** */
+    $('[data-toggle="tooltip"]').tooltip();
 
-	/* ********************************************
-		8. Treeview active
-	******************************************** */
+    /* ********************************************
+        8. Treeview active
+    ******************************************** */
     $("#cat-treeview ul").treeview({
         animated: "normal",
         persist: "location",
@@ -230,33 +230,33 @@
         unique: true,
     });
 
-	/* ********************************************
-		9. Price Slider
-	******************************************** */
-    $( "#slider-range" ).slider({
+    /* ********************************************
+        9. Price Slider
+    ******************************************** */
+    $("#slider-range").slider({
         range: true,
         min: 10,
         max: 500,
-        values: [ 10, 100 ],
-        slide: function( event, ui ) {
+        values: [10, 100],
+        slide: function (event, ui) {
             $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-          
-        }
-        
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    " - $" + $( "#slider-range" ).slider( "values", 1 ) ); 
 
-	/* ********************************************
-		10. Fancybox active
-	******************************************** */
-    $(document).ready(function() {
+        }
+
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+
+    /* ********************************************
+        10. Fancybox active
+    ******************************************** */
+    $(document).ready(function () {
         $('.fancybox').fancybox();
     });
 
-	/* ********************************************
-		11. Elevate Zoom active 
-	******************************************** */    
+    /* ********************************************
+        11. Elevate Zoom active 
+    ******************************************** */
     $("#zoom_03").elevateZoom({
         constrainType: "height",
         zoomType: "lens",
@@ -267,15 +267,15 @@
     });
 
     //pass the images to Fancybox
-    $("#zoom_03").on("click", function(e) {
+    $("#zoom_03").on("click", function (e) {
         var ez = $('#zoom_03').data('elevateZoom');
         $.fancybox(ez.getGalleryList());
         return false;
     });
 
-	/* ********************************************
-		12. single-product-zoom-image carousel
-	******************************************** */ 
+    /* ********************************************
+        12. single-product-zoom-image carousel
+    ******************************************** */
     $('.carousel-btn').slick({
         speed: 700,
         arrows: true,
@@ -285,28 +285,28 @@
         prevArrow: '<button type="button" class="arrow-prev"><i class="zmdi zmdi-long-arrow-left"></i></button>',
         nextArrow: '<button type="button" class="arrow-next"><i class="zmdi zmdi-long-arrow-right"></i></button>',
         responsive: [
-            { breakpoint: 991, settings: { slidesToShow: 3 }  },
-            { breakpoint: 767, settings: { slidesToShow: 3 }  },
-            { breakpoint: 479, settings: { slidesToShow: 3 }  }
+            { breakpoint: 991, settings: { slidesToShow: 3 } },
+            { breakpoint: 767, settings: { slidesToShow: 3 } },
+            { breakpoint: 479, settings: { slidesToShow: 3 } }
         ]
     });
 
-	/* ********************************************
-		13. Cart Plus Minus Button
-	******************************************** */
+    /* ********************************************
+        13. Cart Plus Minus Button
+    ******************************************** */
     $(".cart-plus-minus").prepend('<div class="dec qtybutton">-</div>');
     $(".cart-plus-minus").append('<div class="inc qtybutton">+</div>');
-    $(".qtybutton").on("click", function() {
+    $(".qtybutton").on("click", function () {
         var $button = $(this);
         var oldValue = $button.parent().find("input").val();
         if ($button.text() == "+") {
             var newVal = parseFloat(oldValue) + 1;
-        } 
+        }
         else {
             // Don't allow decrementing below zero
             if (oldValue > 0) {
                 var newVal = parseFloat(oldValue) - 1;
-            } 
+            }
             else {
                 newVal = 0;
             }
@@ -314,11 +314,11 @@
         $button.parent().find("input").val(newVal);
     });
 
-	/* ********************************************
-		14. bootstrap accordion one open at a time
-	******************************************** */ 
-    $('.payment-title a').on('click',function(e){
-        if($(this).parents('.panel').children('.panel-collapse').hasClass('in')){
+    /* ********************************************
+        14. bootstrap accordion one open at a time
+    ******************************************** */
+    $('.payment-title a').on('click', function (e) {
+        if ($(this).parents('.panel').children('.panel-collapse').hasClass('in')) {
             e.stopPropagation();
         }
         // You can also add preventDefault to remove the anchor behavior that makes
@@ -326,24 +326,24 @@
         e.preventDefault();
     });
 
-	/* ********************************************
-		15. Cart tab menu active
-	******************************************** */  
-    $('.cart-tab li a').on("click", function(){
+    /* ********************************************
+        15. Cart tab menu active
+    ******************************************** */
+    $('.cart-tab li a').on("click", function () {
         $(this).addClass("active");
         $(this).parent('li').prevAll('li').find('a').addClass("active");
         $(this).parent('li').nextAll('li').find('a').removeClass("active");
     });
 
-	/* ********************************************
-		16. Blog page manu dropdown 
-	******************************************** */
-    $('.dropdown .option-btn').on('click', function(){
-        if($(this).siblings('.dropdown-menu').hasClass('active')){
+    /* ********************************************
+        16. Blog page manu dropdown 
+    ******************************************** */
+    $('.dropdown .option-btn').on('click', function () {
+        if ($(this).siblings('.dropdown-menu').hasClass('active')) {
             $(this).siblings('.dropdown-menu').removeClass('active').slideUp();
             $(this).removeClass('active');
         }
-        else{
+        else {
             $('.dropdown .dropdown-menu').removeClass('active').slideUp();
             $('.dropdown .option-btn').removeClass('active');
             $(this).addClass('active');
@@ -351,17 +351,19 @@
         }
     });
 
-	/* ********************************************
-		17. Background Toutube Video 
-	******************************************** */
+    /* ********************************************
+        17. Background Toutube Video 
+    ******************************************** */
     $(".youtube-bg").YTPlayer({
-		videoURL:"_OA2oggXehk",
-		containment:'.youtube-bg',
-		mute:true,
-		loop:true,
-	});
+        videoURL: "_OA2oggXehk",
+        containment: '.youtube-bg',
+        mute: true,
+        loop: true,
+    });
 
-
+    $('li#account').hover(function () {
+        $(this).children('ul').toggleClass('dropdown')
+    });
 
 })(jQuery);
 
@@ -371,12 +373,13 @@
 /* ********************************************
     18. STICKY sticky-header
 ******************************************** */
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1){  
-            $('#sticky-header').addClass("sticky");
-        }
-        else{
-            $('#sticky-header').removeClass("sticky");
-        }
-    });
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $('#sticky-header').addClass("sticky");
+    }
+    else {
+        $('#sticky-header').removeClass("sticky");
+    }
+});
 /* ********************************************************* */
+
